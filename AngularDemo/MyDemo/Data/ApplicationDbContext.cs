e9ef432498ee8f9f0d52b6a43aedc670d508741c
Aelisha-Patel <patelaelisha5@gmail.com>
2022-02-25 22:09:43 +0530

    add AspProject,UniversityApp,React and Angular

---
+++ AngularDemo/MyDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyDemo.Models;
+
+namespace MyDemo.Data
+{
+    public class ApplicationDbContext
+        : DbContext
+    {
+
+        public DbSet<Department> Departments { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
